98a3e47e8e904df40c070c5449f2643932a59b7c
Istratie Stefan <109197365+IstratieStefan@users.noreply.github.com>
2024-05-25 15:13:04 +0300

    muie bark

--- Python/main.py
+++ Python/main.py
@@ -1,20 +1,28 @@
-from bark import generate_audio, preload_models
-from scipy.io.wavfile import write as write_wav
-from IPython.display import Audio
+import pyttsx3
+engine = pyttsx3.init() # object creation
 
-# download and load all models
-preload_models()
+""" RATE"""
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 125)     # setting up new voice rate
 
-SAMPLE_RATE = 22050
-# generate audio from text
-text_prompt = """
-     Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
-     But I also have other interests such as playing tic tac toe.
-"""
-audio_array = generate_audio(text_prompt)
 
-# save audio to disk
-write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
+"""VOLUME"""
+volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+print (volume)                          #printing current volume level
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
 
-# play text in notebook
-Audio(audio_array, rate=SAMPLE_RATE)
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[0].id)   #changing index, changes voices. 1 for female
+
+engine.say("plang aici de nervi")
+engine.say('My current speaking rate is ' + str(rate))
+engine.runAndWait()
+engine.stop()
+
+"""Saving Voice to a file"""
+# On linux make sure that 'espeak' and 'ffmpeg' are installed
+engine.save_to_file('Hello World', 'test.mp3')
+engine.runAndWait()
